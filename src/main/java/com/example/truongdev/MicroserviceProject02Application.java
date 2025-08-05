922284a0cca3e003449a0b0b7816da3cececced9
truong minh <mtruongg203@gmail.com>
2025-08-05 10:09:54 +0700

    init commit

---
+++ src/main/java/com/example/truongdev/MicroserviceProject02Application.java
@@ -0,0 +1,13 @@
+package com.example.truongdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProject02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProject02Application.class, args);
+	}
+
+}
