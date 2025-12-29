a84a76883718da67108a5815f70c558aab799158
truong minh <mtruongg203@gmail.com>
2025-12-29 11:58:03 +0700

    style: Update request mappings in controllers to remove 'api' prefix

--- account-service/src/main/java/com/example/accountservice/controller/TransactionController.java
+++ account-service/src/main/java/com/example/accountservice/controller/TransactionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/accounting")
+@RequestMapping("/accounting")
 @RequiredArgsConstructor
 public class TransactionController {
 
